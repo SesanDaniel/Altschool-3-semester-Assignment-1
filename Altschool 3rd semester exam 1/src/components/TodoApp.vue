a12913bfca027a5cf107a99989de14cee786510a
SesanDaniel <obisesan.seyid@gmail.com>
2023-12-13 01:04:04 +0400

    Updated

---
+++ Altschool 3rd semester exam 1/src/components/TodoApp.vue
@@ -0,0 +1,128 @@
+<script>
+export default {
+  data() {
+    return {
+      task: "",
+      editedTask: null,
+      availableStatus: ["To-do", "In-progress", "Completed"],
+      tasks: [
+        {
+          name: "Wash the car",
+          status: "to-do",
+        },
+        {
+          name: "Write Vue code",
+          status: "in-progress",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    submitTask() {
+      if (this.task.length === 0) return;
+
+      if (this.editedTask === null) {
+        this.tasks.push({
+          name: this.task,
+          status: "to-do",
+        });
+      } else {
+        this.tasks[this.editedTask].name = this.task;
+        this.editedTask = null;
+      }
+
+      this.task = "";
+    },
+
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+
+    editTask(index) {
+      this.task = this.tasks[index].name;
+      this.editedTask = index;
+    },
+
+    changeStatus(index) {
+      let newIndex = this.availableStatus.indexOf(this.tasks[index].status);
+      if (++newIndex > 2) newIndex = 0;
+      this.tasks[index].status = this.availableStatus[newIndex];
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="text-center mt-5">Daniel's To-do App</h1>
+
+    <!-- input -->
+
+    <div class="d-flex">
+      <input
+        v-model="task"
+        type="text"
+        placeholder="Write your task"
+        class="form-control"
+      />
+      <button @click="submitTask" class="btn btn-success rounded-0">
+        Submit
+      </button>
+    </div>
+    <!-- Task table -->
+
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Tasks</th>
+          <th scope="col">Status</th>
+          <th scope="col" class="text-center">Edit Task</th>
+          <th scope="col" class="text-center">Delete Task</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>
+            <span :class="{ Done: task.status === 'Completed' }"
+              >{{ task.name }}
+            </span>
+          </td>
+          <td style="width: 120px;">
+            <span
+              :class="{
+                'text-danger': task.status === 'To-do',
+                'text-warning': task.status === 'In-progress',
+                'text-sucess': task.status === 'Completed',
+              }"
+              @click="changeStatus(index)"
+              class="pointer"
+            >
+              {{ task.status }}
+            </span>
+          </td>
+          <td>
+            <div  @click="editTask(index)" class="text-center pointer">
+              <span class="fa fa-pen"></span>
+            </div>
+          </td>
+          <td>
+            <div @click="deleteTask(index)" class="text-center pointer">
+              <span class="fa fa-trash"></span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+.pointer {
+  cursor: pointer;
+}
+
+.Done {
+  text-decoration: line-through;
+}
+</style>
